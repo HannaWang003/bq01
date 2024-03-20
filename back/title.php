0c2755c211699a142bed0110a4e0ed098ab15086
HannaWang003 <hannawang003@gmail.com>
2024-03-20 20:25:03 +0800

    變更css.style, menu的背景圖路徑

--- back/title.php
+++ back/title.php
@@ -14,16 +14,14 @@
                 $rows = $Title->all();
                 foreach ($rows as $row) {
                 ?>
-                <tr style="text-align:center">
-                    <td width="45%"><img src="./img/<?= $row['img'] ?>" style="width:300px;height:30px"></td>
-                    <td width="23%"><input type="text" name="text[]" value="<?= $row['text'] ?>"></td>
-                    <td width="7%"><input type="checkbox" name="sh[]" value="<?= $row['id'] ?>"
-                            <?= ($row['sh'] == 1) ? "checked" : "" ?>></td>
-                    <td width="7%"><input type="checkbox" name="del[]" value="<?= $row['id'] ?>" id=""></td>
-                    <td><input type="button" onclick="op('#cover','#cvr','./back/edit_title.php')"
-                            value="更新圖片"></button></td>
-                </tr>
-                <input type="hidden" name="id[]" value="<?= $row['id'] ?>">
+                    <tr style="text-align:center">
+                        <td width="45%"><img src="./img/<?= $row['img'] ?>" style="width:300px;height:30px"></td>
+                        <td width="23%"><input type="text" name="text[]" value="<?= $row['text'] ?>"></td>
+                        <td width="7%"><input type="radio" name="sh[]" value="<?= $row['id'] ?>" <?= ($row['sh'] == 1) ? "checked" : "" ?>></td>
+                        <td width="7%"><input type="checkbox" name="del[]" value="<?= $row['id'] ?>" id=""></td>
+                        <td><input type="button" onclick="op('#cover','#cvr','./back/edit_title.php')" value="更新圖片"></button></td>
+                    </tr>
+                    <input type="hidden" name="id[]" value="<?= $row['id'] ?>">
                 <?php
                 }
                 ?>
@@ -33,8 +31,7 @@
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./back/add_title.php')"
-                            value="新增網站標題圖片">
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./back/add_title.php')" value="新增網站標題圖片">
                     </td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
                     </td>
@@ -44,9 +41,9 @@
     </form>
 </div>
 <script>
-function logout() {
-    $.post('./api/logout.php', function(res) {
-        location.href = "./index.php";
-    })
-}
+    function logout() {
+        $.post('./api/logout.php', function(res) {
+            location.href = "./index.php";
+        })
+    }
 </script>
