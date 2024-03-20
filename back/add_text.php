acd22a537dbe18731e5014fdc7b242c6419d4072
HannaWang003 <hannawang003@gmail.com>
2024-03-20 21:27:57 +0800

    vertical-align:top_讓inline的elm靠上對齊

--- back/add_text.php
+++ back/add_text.php
@@ -1,9 +1,13 @@
 <?php
-$type = ($_GET['do'] == 'ad') ? "動態文字廣告" : "最新消息資料"
+$type = ($_GET['do'] == 'ad') ? "動態文字廣告" : "最新消息資料";
 ?>
 <h2 class="cent">新增<?= $type ?></h2>
 <hr>
 <form action="./api/add_text.php?do=<?= $_GET['do'] ?>" method="post">
-    <div class="cent"><span><?= $type ?>:</span><input type="text" name="text"></div>
+    <div class="cent"><span style='vertical-align:top'><?= $type ?>:</span>
+        <?php
+        echo ($_GET['do'] == 'ad') ? "<input type='text' name='text'>" : "<textarea name='text' style='width:50%;height:100px;'></textarea>"
+        ?>
+    </div>
     <div class="cent"><input type="submit" value="新增"><input type="reset" value="重置"></div>
 </form>
--- back/news.php
+++ back/news.php
@@ -15,15 +15,14 @@ $type = "最新消息資料";
                 $rows = $News->all();
                 foreach ($rows as $row) {
                 ?>
-                <tr>
-                    <td width="23%"><input type="text" name="text[]" value="<?= $row['text'] ?>" style="width:80%;">
-                    </td>
-                    <td class="cent" width="7%"><input type="checkbox" name="sh[]" value="<?= $row['id'] ?>"
-                            <?= ($row['sh'] == 1) ? "checked" : "" ?>></td>
-                    <td class="cent" width="7%"><input type="checkbox" name="del[]" value="<?= $row['id'] ?>" id="">
-                    </td>
-                </tr>
-                <input type="hidden" name="id[]" value="<?= $row['id'] ?>">
+                    <tr>
+                        <td width="23%"><textarea name="text[]" style="width:90%;height:50px;"><?= $row['text'] ?></textarea>
+                        </td>
+                        <td class="cent" width="7%"><input type="checkbox" name="sh[]" value="<?= $row['id'] ?>" <?= ($row['sh'] == 1) ? "checked" : "" ?>></td>
+                        <td class="cent" width="7%"><input type="checkbox" name="del[]" value="<?= $row['id'] ?>" id="">
+                        </td>
+                    </tr>
+                    <input type="hidden" name="id[]" value="<?= $row['id'] ?>">
                 <?php
                 }
                 ?>
@@ -33,9 +32,7 @@ $type = "最新消息資料";
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <td width="200px"><input type="button"
-                            onclick="op('#cover','#cvr','./back/add_text.php?do=<?= $_GET['do'] ?>')"
-                            value="新增<?= $type ?>">
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./back/add_text.php?do=<?= $_GET['do'] ?>')" value="新增<?= $type ?>">
                     </td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
                     </td>
